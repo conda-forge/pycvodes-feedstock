5e4e77f817ac22aaef50002f78611cd73c6ef117
Björn Dahlgren <bjodah@gmail.com>
2018-07-20 10:21:45 +0200

    Reorder libraries

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,3 @@
 set PYCVODES_LAPACK=0
-set PYCVODES_SUNDIALS_LIBS="sundials_nvecserial,sundials_cvodes"
+set PYCVODES_SUNDIALS_LIBS="sundials_cvodes,sundials_nvecserial"
 python -m pip install --no-deps --ignore-installed .
