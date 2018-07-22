2acb5bce96707d3550a7cf5f65ca8ea6e89449d9
Björn Dahlgren <bjodah@gmail.com>
2018-07-22 16:26:22 +0200

    Fix typo

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,7 +10,7 @@ export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial
 cat <<EOF>pycvodes/_config.py
 env = {
     'LAPACK': "${PYCVODES_LAPACK}",
-    'SUNDIALS_LIBS': "${PYCVODES_SUNDIALS_LIBS}"
+    'SUNDIALS_LIBS': "${PYCVODES_SUNDIALS_LIBS}",
     'NO_KLU': '0'
 }
 EOF
