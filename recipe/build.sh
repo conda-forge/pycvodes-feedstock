abba654832278fad11553cfceafdd68f30b6c693
Björn Dahlgren <bjodah@gmail.com>
2018-08-03 12:47:53 +0200

    More config variables in 0.11.5

--- recipe/build.sh
+++ recipe/build.sh
@@ -12,6 +12,10 @@ env = {
     'LAPACK': "${PYCVODES_LAPACK}",
     'SUNDIALS_LIBS': "${PYCVODES_SUNDIALS_LIBS}",
     'NO_KLU': '0'
+    'NO_LAPACK': '0',
+    'SUNDIALS_PRECISION': 'double',
+    'REAL_TYPE': 'double',
+    'INDEX_TYPE': 'int32_t'
 }
 EOF
 export PYCVODES_STRICT=1
