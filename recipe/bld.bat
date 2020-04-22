9e384d1ebb63caebb239c1a4d230f0d7e702ea6c
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-04-22 11:46:49 +0200

    Update build scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,4 @@
-set "PYCVODES_LAPACK=0"
+set "PYCVODES_NO_KLU=1"
+set "PYCVODES_NO_LAPACK=1"
 set "PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial"
 python -m pip install --no-deps --ignore-installed .
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
 export PYCVODES_LAPACK=lapack,blas
-
-# Sundials 2.7:
-#export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial
-
-# Sundials 3.x:
 export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_sunlinsollapackdense,sundials_sunlinsollapackband,sundials_sunlinsolklu
-
-cat <<EOF>pycvodes/_config.py
-env = {
-    'LAPACK': "${PYCVODES_LAPACK}",
-    'SUNDIALS_LIBS': "${PYCVODES_SUNDIALS_LIBS}",
-    'NO_KLU': '0'
-}
-EOF
-export PYCVODES_STRICT=1
 python -m pip install --no-deps --ignore-installed . -vv
