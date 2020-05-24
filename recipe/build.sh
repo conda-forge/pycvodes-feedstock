cb6a4510d8a453ed95f32b32c1bd8380b0897057
Björn Ingvar Dahlgren <bjodah@gmail.com>
2020-05-24 16:07:16 +0200

    retrigger build (logs too old)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 export PYCVODES_LAPACK=lapack,blas
 export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_sunlinsollapackdense,sundials_sunlinsollapackband,sundials_sunlinsolklu
+
 python -m pip install --no-deps --ignore-installed . -vv
