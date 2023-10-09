7dbf4820a5970b75e5ea5057c26c67c7b0580ae6
Björn Ingvar Dahlgren <bjodah@gmail.com>
2023-10-09 09:31:35 +0200

    Make run_test.py more verbose to aid troubleshooting

--- recipe/run_test.py
+++ recipe/run_test.py
@@ -2,10 +2,24 @@
 # -*- coding: utf-8 -*-
 
 import os
-from pycvodes import get_include
-header_fname = 'cvodes_cxx.hpp'
-assert header_fname in os.listdir(get_include())
-path = os.path.join(get_include(), header_fname)
-assert open(path, 'rt').readline().startswith('#pragma once')
 import pytest
-pytest.main(['--pyargs', 'pycvodes'])
+import sys
+from pycvodes import get_include
+
+
+def check_for_header_file():
+    header_fname = 'cvodes_cxx.hpp'
+    assert header_fname in os.listdir(get_include())
+    print("OK: Found header file.")
+    path = os.path.join(get_include(), header_fname)
+    assert open(path, 'rt').readline().startswith('#pragma once')
+    print("OK: Header is readable and starts with '#pragma once'.")
+
+if __name__ == '__main__':
+    check_for_header_file()
+    # https://docs.pytest.org/en/7.3.x/how-to/usage.html#calling-pytest-from-python-code
+    pytest_args = ['--pyargs', 'pycvodes', '-v', '-s']
+    print("INFO: about to run pytest.main(...) with args: %s" % str(pytest_args))
+    pytest_exit_code = pytest.main()
+    print("INFO: pytest.main(...) returned exit code: %d" % pytest_exit_code)
+    sys.exit(pytest_exit_code)
