0a505270263e495309020796a985e42f0de4319e
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-04-16 20:52:36 +0200

    Attempt to fix <klu.h> not found

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PYCVODES_LAPACK=lapack,blas
 export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_sunlinsollapackdense,sundials_sunlinsollapackband,sundials_sunlinsolklu
-
+export CPATH=${CONDA_PREFIX}/include/suitesparse  # sundials' sunlinsol_klu.h has: #include <klu.h>
 python -m pip install --no-deps --ignore-installed . -vv
