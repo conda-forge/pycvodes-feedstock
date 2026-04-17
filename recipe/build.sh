1054d0c35152b0d5b003bd719d7d173475d05228
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-04-17 09:37:38 +0200

    Use PREFIX rather than CONDA_PREFIX in build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 export PYCVODES_LAPACK=lapack,blas
 export PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_sunlinsollapackdense,sundials_sunlinsollapackband,sundials_sunlinsolklu
-find $PREFIX -name klu.h
 export CPATH=${PREFIX}/include/suitesparse  # sundials' sunlinsol_klu.h has: #include <klu.h>
-echo $CPATH
-exit 1
 python -m pip install --no-deps --ignore-installed . -vv
