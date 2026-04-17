6f533640cfc0e0070545bef198aa29679a5b93c7
Björn Ingvar Dahlgren <bjodah@gmail.com>
2026-04-17 14:47:54 +0200

    Try adding sundials_core lib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
 set "PYCVODES_NO_KLU=1"
 set "PYCVODES_NO_LAPACK=1"
-set "PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial"
+set "PYCVODES_SUNDIALS_LIBS=sundials_cvodes,sundials_nvecserial,sundials_core"
 python -m pip install --no-deps --ignore-installed .
